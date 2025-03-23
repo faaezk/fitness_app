de1fe6015fcc43a55b30716dbad136b9b11600da
Faaez Kamal <faaez.kamal@gmail.com>
2025-03-23 13:59:34 +0000

    cleaned up arduino code

--- arduino/StepClassifier/StepClassifier.ino
+++ arduino/StepClassifier/StepClassifier.ino
@@ -8,34 +8,21 @@
 #include "stepmodel.h"
 
 const int numSamples = 20;
-
 int samplesRead = 0;
 
-// pull in all the TFLM ops, you can remove this line and
-// only pull in the TFLM ops you need, if would like to reduce
-// the compiled size of the sketch.
 tflite::AllOpsResolver tflOpsResolver;
-
 const tflite::Model* tflModel = nullptr;
 tflite::MicroInterpreter* tflInterpreter = nullptr;
 TfLiteTensor* tflInputTensor = nullptr;
 TfLiteTensor* tflOutputTensor = nullptr;
 
-// Create a static memory buffer for TFLM, the size may need to
-// be adjusted based on the model you are using
 constexpr int tensorArenaSize = 8 * 1024;
 byte tensorArena[tensorArenaSize] __attribute__((aligned(16)));
 
-// array to map gesture index to a name
-const char* GESTURES[] = {
-  "walk",
-  "run"
-};
-
+const char* GESTURES[] = {"walk", "run"};
 #define NUM_GESTURES (sizeof(GESTURES) / sizeof(GESTURES[0]))
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   while (!Serial);
 
@@ -44,13 +31,6 @@ void setup() {
     Serial.println("Failed to initialize IMU!");
     while (1);
   }
-
-  // print out the samples rates of the IMUs
-  Serial.print("Accelerometer sample rate = ");
-  Serial.print(IMU.accelerationSampleRate());
-  Serial.println(" Hz");
-
-  Serial.println();
 
   // get the TFL representation of the model byte array
   tflModel = tflite::GetModel(model);
@@ -62,7 +42,6 @@ void setup() {
   // Create an interpreter to run the model
   tflInterpreter = new tflite::MicroInterpreter(tflModel, tflOpsResolver, tensorArena, tensorArenaSize);
 
-
   // Allocate memory from the tensor_arena for the model's tensors.
   TfLiteStatus allocate_status = tflInterpreter->AllocateTensors();
   if (allocate_status != kTfLiteOk) {
@@ -73,30 +52,22 @@ void setup() {
   // Get pointers for the model's input and output tensors
   tflInputTensor = tflInterpreter->input(0);
   tflOutputTensor = tflInterpreter->output(0);
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   float aX, aY, aZ;
 
-  if (samplesRead < numSamples) {
-    // check if new acceleration data is available
-    if (IMU.accelerationAvailable()) {
-      // read the acceleration
-      IMU.readAcceleration(aX, aY, aZ);
-
-      // normalize the IMU data between 0 to 1 and store in the model's
-      // input tensor
-      tflInputTensor->data.f[samplesRead * 3 + 0] = aX;
-      tflInputTensor->data.f[samplesRead * 3 + 1] = aY;
-      tflInputTensor->data.f[samplesRead * 3 + 2] = aZ;
-      samplesRead++;
-    }
+  // normalize the IMU data between 0 to 1 and store in the model's input tensor
+  if (samplesRead < numSamples && IMU.accelerationAvailable()) {
+    IMU.readAcceleration(aX, aY, aZ);
+    tflInputTensor->data.f[samplesRead * 3 + 0] = aX;
+    tflInputTensor->data.f[samplesRead * 3 + 1] = aY;
+    tflInputTensor->data.f[samplesRead * 3 + 2] = aZ;
+    samplesRead++;
   }
-
+	
+  // Run inferencing
   if (samplesRead == numSamples) {
-    // Run inferencing
     TfLiteStatus invokeStatus = tflInterpreter->Invoke();
     if (invokeStatus != kTfLiteOk) {
       Serial.println("Invoke failed!");
@@ -109,18 +80,12 @@ void loop() {
       Serial.print(GESTURES[i]);
       Serial.print(": ");
       Serial.println(tflOutputTensor->data.f[i], 6);
-
-      // if (tflOutputTensor->data.f[i] > 0.5) {
-      //   Serial.println("running");
-      // }else if (tflOutputTensor->data.f[i] < 0.5){
-      //   Serial.println("walking");
-      // }
     }
 
-    if (tflOutputTensor->data.f[2] > 0.5) {
-        Serial.println("running");
-    }else {
-        Serial.println("walking");
+    if (tflOutputTensor->data.f[0] < 0.6) {
+      Serial.println("running");
+    } else {
+      Serial.println("walking");
     }
 
     // Clean up the data buffer before filling up for the next batch.
@@ -131,6 +96,6 @@ void loop() {
 
     Serial.println();
     samplesRead = 0;
+    delay(100);
   }
-
 }
